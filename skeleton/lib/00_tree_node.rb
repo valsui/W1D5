155207060f0f192ae69f79091bffbe1707e3db61
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-04-20 13:55:52 -0700

    Add add_child and remove_child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -23,4 +23,11 @@ class PolyTreeNode
       end
     end 
     
+    def add_child(child_node)
+      child_node.parent = self 
+    end
+    
+    def remove_child(child_node)
+      child_node.parent = nil 
+    end 
 end
