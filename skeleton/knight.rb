419ad8131b9cede9bb3d196f280c612cdf870df6
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-04-20 16:18:45 -0700

    Implement valid moves class method

--- skeleton/knight.rb
+++ skeleton/knight.rb
@@ -1,13 +1,26 @@
 class KnightPathFinder
 
-  def inititalize(pos)
+  def initialize(pos)
     @init_pos = pos
   end
 
   def self.valid_moves(pos)
-
+    ans = []
+    numbers = (-2..2).to_a - [0]
+    numbers.each do |i|
+      numbers.each do |j|
+        if i.abs + j.abs == 3
+          current = [pos[0] + i, pos[1] + j]
+          ans << current if current.all? {|x| x.between?(0,7)}
+        end
+      end
+    end
+    ans
   end
 
+  def print_moves
+    p self.class.valid_moves
+  end
 
   def new_move_postions
 
